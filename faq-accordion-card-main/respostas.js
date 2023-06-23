0c7ebfd974dd829176449afe4e7f6c27796cc297
cacovidigal <marcosmsvidigal@gmail.com>
2023-06-23 12:04:43 -0300

    primeira logica de js feita

---
+++ faq-accordion-card-main/respostas.js
@@ -0,0 +1,27 @@
+function exibirResposta_1() {
+  var resposta_1 = "You can invite up to 2 additional users on the Free plan. There is no limit on team members for the Premium plan.";
+    document.getElementById("resposta_1").innerHTML = resposta_1;
+}
+
+function exibirResposta_2() {
+    var resposta_2 = "No more than 2GB. All files in your account must fit your allotted storage space.";
+      document.getElementById("resposta_2").innerHTML = resposta_2;
+  }
+
+function exibirResposta_3() {
+    var resposta_3 = "Forgot password” from the login page or “Change password” from your profile page. A reset link will be emailed to you.";
+      document.getElementById("resposta_3").innerHTML = resposta_3;
+  }
+
+function exibirResposta_4() {
+    var resposta_4 = "Yes! Send us a message and we will process your request no questions asked.";
+      document.getElementById("resposta_4").innerHTML = resposta_4;
+  }
+
+function exibirResposta_5() {
+    var resposta_5 = "Chat and email support is available 24/7. Phone lines are open during normal business hours.";
+      document.getElementById("resposta_5").innerHTML = resposta_5;
+  }
+
+     
+            
